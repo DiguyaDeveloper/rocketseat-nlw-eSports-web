0ac72c7dbd7dd320b84598a1252ed436e9c0a100
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-13 20:48:46 -0300

    feat: :sparkles: first updates in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button> {props.title} </button>;
+}
+
 function App() {
-  return <h1>Hellow World</h1>;
+  return (
+    <>
+      <Button title="Teste 1"></Button>
+      <Button title="Teste 2"></Button>
+      <Button title="Teste 3"></Button>
+      <Button title="Hellow World"></Button>
+    </>
+  );
 }
 
 export default App;
