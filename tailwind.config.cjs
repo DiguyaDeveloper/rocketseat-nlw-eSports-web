e218d066df6c419524e64a24dc3ec69cf7b87ef8
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-14 23:03:40 -0300

    feat(aula-2): :sparkles:
    Introduce background image in root html

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx'
+    './src/**/*.tsx',
+    './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')"
+      }
+    },
   },
   plugins: [],
 }
