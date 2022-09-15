5913eb03af1b5092b5866a54c7673d3131018278
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-14 22:50:04 -0300

    feat(aula-2): :sparkles:
    clean app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return <button> {props.title} </button>;
-}
-
 function App() {
-  return (
-    <>
-      <Button title="Teste 1"></Button>
-      <Button title="Teste 2"></Button>
-      <Button title="Teste 3"></Button>
-      <Button title="Hellow World"></Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
