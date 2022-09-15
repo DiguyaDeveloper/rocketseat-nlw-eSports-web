a57a196ba26b32ee33a03fd3273f42003a82a112
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-14 23:15:18 -0300

    feat(aula-2): :sparkles:
    Create title and create new color gradient with tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./styles/main.css";
+import logoImg from "./assets/logo-esports-nlw.svg";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+        <img src={logoImg} alt="" />
+        <h1 className="text-6xl text-white font-black mt-20">
+          Seu{" "}
+          <span className="text-transparent bg-nlw-gradient bg-clip-text">
+            duo
+          </span>{" "}
+          está aqui.
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-    './index.html'
-  ],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/background-galaxy.png')"
-      }
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572fc 20.08%, #45e7ad 39.94%, #e1055e 75.57%)",
+        galaxy: "url('/background-galaxy.png')",
+      },
     },
   },
   plugins: [],
-}
+};
