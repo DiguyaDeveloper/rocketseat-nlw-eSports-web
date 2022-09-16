31df2339e32fe2b70cd0b87f9e1d4859ad6e31fe
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-15 21:05:00 -0300

    feat(aula-2): :sparkles:
    Introduce game-gradient

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
       backgroundImage: {
         "nlw-gradient":
           "linear-gradient(89.86deg, #9572fc 20.08%, #45e7ad 39.94%, #e1055e 75.57%)",
+        "game-gradient":
+          "linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%",
         galaxy: "url('/background-galaxy.png')",
       },
     },
