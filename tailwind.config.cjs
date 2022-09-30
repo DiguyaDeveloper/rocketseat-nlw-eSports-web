77d95ed9b2e0bcbc7e230d17943174d89c901bfd
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-30 20:01:00 -0300

    fix(aula-4): Fix bug in game-gradient bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,7 @@ module.exports = {
         "nlw-gradient":
           "linear-gradient(89.86deg, #9572fc 20.08%, #45e7ad 39.94%, #e1055e 75.57%)",
         "game-gradient":
-          "linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%",
+          "linear-gradient(188deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.88%)",
         galaxy: "url('/background-galaxy.png')",
       },
     },
