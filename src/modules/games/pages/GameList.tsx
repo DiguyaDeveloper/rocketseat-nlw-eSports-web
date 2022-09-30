aafdff387ba7b5c0ac1db722083bd7e63d39448f
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-09-30 18:48:37 -0300

    feat(aula-4): :sparkles: Introduce fetch of get games list

--- src/modules/games/pages/GameList.tsx
+++ src/modules/games/pages/GameList.tsx
@@ -1,17 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GameBanner } from "../components/GameBanner";
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    Ads: number;
+  };
+}
+
 export function GameList() {
-  useEffect(() => {}, []);
+  const [games, setGames] = useState<Game[]>([]);
+
+  const getGames = (): void => {
+    fetch("http://localhost:3333/games")
+      .then((response) => response.json())
+      .then((data: Game[]) => {
+        setGames(data);
+      });
+  };
+
+  useEffect(getGames, []);
 
   return (
     <div className="grid grid-cols-6 gap-6 mt-16">
-      <GameBanner
-        key={1}
-        bannerUrl={"/game-1.png"}
-        title={"League of legends"}
-        adsCount={5}
-      />
+      {games &&
+        games.map((game: Game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.Ads}
+            />
+          );
+        })}
     </div>
   );
 }
