ba17e090c4b72cc70c65a7a5fd1545167011bf25
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-05 23:34:49 -0300

    feat(aula-5): :sparkles: Update Ads Create Form
    Update Checkbox component
    Add name inputs
    Create handleSubmit form
    Create states to useVoiceChannel, weekDays, selectedGame

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -4,12 +4,16 @@ import { InputHTMLAttributes } from "react";
 
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   title: string;
+  onCheckedChange: (value: boolean) => void;
 }
 
 export function RadixCheckbox(props: CheckboxProps) {
   return (
     <label className="flex items-center gap-2 text-sm cursor-pointer">
-      <Checkbox.Root className="w-6 h-6 p-1 rounded bg-zinc-900">
+      <Checkbox.Root
+        onCheckedChange={props.onCheckedChange}
+        className="w-6 h-6 p-1 rounded bg-zinc-900"
+      >
         <Checkbox.Indicator>
           <Check className="w-4 h-4 text-emerald-400"></Check>
         </Checkbox.Indicator>
--- src/modules/ads/components/AdsCreateForm/AdsCreateForm.tsx
+++ src/modules/ads/components/AdsCreateForm/AdsCreateForm.tsx
@@ -2,15 +2,17 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { GameController } from "phosphor-react";
 import { Input } from "../../../../components/Input/Input";
 import { RadixSelect } from "../../../../components/select/Select";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { Game } from "../../../games/interfaces/game.interface";
 import { RadixMultipleToggleGroup } from "../../../../components/multiple-toggle-group/MultipleToggleGroup";
 import { weekDaysConstant } from "../../../../shared/constants/week-days.constant";
 import { RadixCheckbox } from "../../../../components/checkbox/Checkbox";
 
 export function AdsCreateForm() {
-  const [selectedGame, setSelectedGame] = useState<string>();
   const [games, setGames] = useState<Game[]>([]);
+  const [selectedGame, setSelectedGame] = useState<string>();
+  const [weekDays, setWeekDays] = useState<string[]>();
+  const [useVoiceChannel, setUseVoiceChannel] = useState<boolean>(false);
 
   const getGames = (): void => {
     fetch("http://localhost:3333/games")
@@ -22,8 +24,18 @@ export function AdsCreateForm() {
 
   useEffect(getGames, []);
 
+  const handleCreateAds = (event: FormEvent) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement);
+    console.log("formData", Object.fromEntries(formData));
+    console.log("selectedGame", selectedGame);
+    console.log("weekDays", weekDays);
+    console.log("useVoiceChannel", useVoiceChannel);
+  };
+
   return (
-    <form className="mt-8 flex flex-col gap-4">
+    <form onSubmit={handleCreateAds} className="mt-8 flex flex-col gap-4">
       <div className="flex flex-col gap-2">
         <label htmlFor="game" className="font-semibold">
           Qual o game?
@@ -44,17 +56,25 @@ export function AdsCreateForm() {
 
       <div>
         <label htmlFor="name">Seu nome (ou nickname)</label>
-        <Input id="name" placeholder="Como te chamam dentro do game?" />
+        <Input
+          id="name"
+          name="name"
+          placeholder="Como te chamam dentro do game?"
+        />
       </div>
 
       <div className="grid grid-cols-2 gap-4">
         <div className="flex flex-col gap-2">
           <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-          <Input id="yearsPlaying" placeholder="Tudo bem ser ZERO" />
+          <Input
+            id="yearsPlaying"
+            name="yearsPlaying"
+            placeholder="Tudo bem ser ZERO"
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="discord">Qual seu discord?</label>
-          <Input id="discord" placeholder="Usuario#0000" />
+          <Input id="discord" name="discord" placeholder="Usuario#0000" />
         </div>
       </div>
 
@@ -64,7 +84,8 @@ export function AdsCreateForm() {
 
           <RadixMultipleToggleGroup
             id="weekDays"
-            onValueChange={() => {}}
+            name="weekDays"
+            onValueChange={(weekDays: string[]) => setWeekDays(weekDays)}
             items={weekDaysConstant.map(({ title, label, value }) => {
               return { title, value, label };
             })}
@@ -73,8 +94,13 @@ export function AdsCreateForm() {
         <div className="flex flex-col gap-2 flex-1">
           <label htmlFor="weekDays">Qual horário do dia?</label>
           <div className="grid grid-cols-2 gap-2">
-            <Input id="hourStart" type="time" placeholder="De" />
-            <Input id="hourEnd" type="time" placeholder="Até" />
+            <Input
+              id="hourStart"
+              name="hourStart"
+              type="time"
+              placeholder="De"
+            />
+            <Input id="hourEnd" name="hourEnd" type="time" placeholder="Até" />
           </div>
         </div>
       </div>
@@ -82,6 +108,7 @@ export function AdsCreateForm() {
       <RadixCheckbox
         className="mt-2"
         title="Costumo me conectar ao chat de voz"
+        onCheckedChange={(checked) => setUseVoiceChannel(checked === true)}
       />
 
       <footer className="mt-4 flex justify-end gap-4">
