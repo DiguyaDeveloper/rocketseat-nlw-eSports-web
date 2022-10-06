a674878e7b1cce5f88d1cf198e707cefd568b750
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-05 22:55:46 -0300

    feat(aula-5): :sparkles: Create custom multiple toggle group
    Create custom component with radix ui

---
+++ src/components/multiple-toggle-group/MultipleToggleGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
+
+interface ToggleGroupProps {
+  items: { value: string; title: string }[];
+  onValueChange: (value: string[]) => void;
+}
+
+export function RadixMultipleToggleGroup({
+  onValueChange,
+  items,
+  ...props
+}: ToggleGroupProps) {
+  const [values, setValues] = useState<string[]>();
+  return (
+    <ToggleGroup.Root
+      type="multiple"
+      className="grid grid-cols-2 gap-2"
+      onValueChange={(items) => {
+        setValues(items);
+        onValueChange(items);
+      }}
+    >
+      {items.map((item, index) => {
+        return (
+          <ToggleGroup.Item
+            key={index}
+            value={item.value}
+            title={item.title}
+            className="w-8 h-8 rounded bg-zinc-900"
+          >
+            {item.value}
+          </ToggleGroup.Item>
+        );
+      })}
+    </ToggleGroup.Root>
+  );
+}
