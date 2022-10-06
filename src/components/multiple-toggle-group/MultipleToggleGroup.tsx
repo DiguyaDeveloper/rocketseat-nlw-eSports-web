e6d0b15e688febd29421ace3bb01455d5184b2d7
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-05 23:01:05 -0300

    feat(aula-5): :sparkles: Add label to props in toggle group component

--- src/components/multiple-toggle-group/MultipleToggleGroup.tsx
+++ src/components/multiple-toggle-group/MultipleToggleGroup.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
 
 interface ToggleGroupProps {
-  items: { value: string; title: string }[];
+  items: { value: string; title: string; label: string }[];
   onValueChange: (value: string[]) => void;
 }
 
@@ -29,7 +29,7 @@ export function RadixMultipleToggleGroup({
             title={item.title}
             className="w-8 h-8 rounded bg-zinc-900"
           >
-            {item.value}
+            {item.label}
           </ToggleGroup.Item>
         );
       })}
