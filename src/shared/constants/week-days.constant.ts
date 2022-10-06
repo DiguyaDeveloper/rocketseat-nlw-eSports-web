ba6b750762386cb7d5fe54ac47dee7dac70c920e
DiguyaDeveloper <diegocecconcwb@outlook.com>
2022-10-05 22:56:10 -0300

    feat(aula-5): :sparkles: Create days of week constant

---
+++ src/shared/constants/week-days.constant.ts
@@ -0,0 +1,38 @@
+export const weekDaysConstant: { title: string; value: string; day: number }[] =
+  [
+    {
+      title: "Domingo",
+      value: "D",
+      day: 1,
+    },
+    {
+      title: "Segunda",
+      value: "S",
+      day: 2,
+    },
+    {
+      title: "Terça",
+      value: "T",
+      day: 3,
+    },
+    {
+      title: "Quarta",
+      value: "Q",
+      day: 4,
+    },
+    {
+      title: "Quinta",
+      value: "Q",
+      day: 5,
+    },
+    {
+      title: "Sexta",
+      value: "S",
+      day: 6,
+    },
+    {
+      title: "Sabado",
+      value: "S",
+      day: 7,
+    },
+  ];
